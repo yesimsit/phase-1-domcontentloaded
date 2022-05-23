cfae5e8a6009fda779ee63524d27076b7e0d2e54
yesim <sityesim@gmail.com>
2022-05-23 10:15:58 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+    const text = document.getElementById("text")
+    text.innerText = "This is really cool!"
+})
